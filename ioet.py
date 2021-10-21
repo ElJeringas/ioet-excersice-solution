049177befc6da56fdbe4cd9df5b36988867a5827
ElJeringas <santiagojss32@gmail.com>
2021-10-21 17:25:46 -0500

    using pandas

--- ioet.py
+++ ioet.py
@@ -1,32 +1,21 @@
 
-import csv;
-import pandas as pd
-pd.read_csv
+import csv; #csv library
+import pandas as pd #import pandas library for data handle
+cont  = 0
+with open('c:/Users/Santiago/Desktop/SantiagoSarmiento_Ioet/schedule.txt',newline='') as file:  #open the .txt file going to the directory  and name this variable 'file'
+    csv_reader = csv.DictReader(file, delimiter=',',) #convert the .txt in csv format, this will transform the text to table
+    df = pd.DataFrame(csv_reader) #return object wiht rows and columns.
 
-with open('c:/Users/Santiago/Desktop/SantiagoSarmiento_Ioet/schedule.txt',newline='') as file:
-    csv_reader = csv.DictReader(file, delimiter=',',)
-    """ line_count = 0 """
-    df = pd.DataFrame(csv_reader) 
-
-    """ mondays = (df.loc[df['monday']!='free',["name","monday"]]) """
+    mondays = (df.loc[df['monday']!='free',["name","monday"]]) #separa al dia lunes y muestra los nombres de trabajadores y horario, excepto si esta "free"
     """     print(mondays['monday'].isin(['10:00-12:00']))"""
-    df_mon=df["name","monday"]
-    data_frame = df_mon.set_index('monday')
-    df_monday = data_frame[(data_frame.index > '10:00') & (data_frame.index <= '12:00')]
+    data_frame = mondays.set_index('monday') #transforma la cadena de texto string a un conjunto de numeros (formato hora)
+    df_monday = data_frame[(data_frame.index > '10:00') & (data_frame.index <= '12:00')] # compara si hay valores dentro del dia que estan dentro de las horas 10:00-12:00 y muestra si es que existen cruces de trabajadores, sino no los muestra
     print (df_monday)
+    print('-----------------------')
+    tuesday = (df.loc[df['tuesday']!='free',["name","tuesday"]]) #separa al dia lunes y muestra los nombres de trabajadores y horario, excepto si esta "free"
+    data_frame_tuesday = tuesday.set_index('tuesday') #transforma la cadena de texto string a un conjunto de numeros (formato hora)
+    df_tuesday = data_frame_tuesday[(data_frame_tuesday.index > '10:00') & (data_frame_tuesday.index <= '12:00')] # compara si hay valores dentro del dia que estan dentro de las horas 10:00-12:00 y muestra si es que existen cruces de trabajadores, sino no los muestra
+    print (df_tuesday)
 
 
-"""     print('-------------')
-    print(df.loc[df['tuesday']!='free',["name","tuesday"]]) 
-    print('-------------')
-    print(df.loc[df['wednesday']!='free',["name","wednesday"]])
-    print('-------------')
-    print(df.loc[df['thursday']!='free',["name","thursday"]]) 
-    print('-------------')
-    print(df.loc[df['friday']!='free',["name","friday"]]) 
-    print('-------------')
-    print(df.loc[df['saturday']!='free',["name","saturday"]]) 
-    print('-------------')
-    print(df.loc[df['sunday']!='free',["name","sunday"]]) 
- """
         
