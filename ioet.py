8efbd70d42e6ad9457cc2d2e547dc86c28c7f0a5
ElJeringas <santiagojss32@gmail.com>
2021-10-22 15:21:38 -0500

    solving of the excercise using csv and datetime

--- ioet.py
+++ ioet.py
@@ -1,21 +1,31 @@
 
-import csv; #csv library
-import pandas as pd #import pandas library for data handle
-cont  = 0
+import csv
+from datetime import datetime
+
+
+rows = []
+employees=[]
+
+
+
 with open('c:/Users/Santiago/Desktop/SantiagoSarmiento_Ioet/schedule.txt',newline='') as file:  #open the .txt file going to the directory  and name this variable 'file'
-    csv_reader = csv.DictReader(file, delimiter=',',) #convert the .txt in csv format, this will transform the text to table
-    df = pd.DataFrame(csv_reader) #return object wiht rows and columns.
+    csv_reader = csv.reader(file, delimiter=',') #convert the .txt in csv format, this will transform the text to table
+    #header=next(csv_reader)
+    for row in csv_reader:
+        employe={} #diccionario
+        for field in row:
+            if("-" in field):
+                employe[field[0:2]+'_start'] = datetime.strptime(field.split('-')[0][2:], '%H:%M').strftime('%I:%M%p')
+                employe[field[0:2]+'_end'] = datetime.strptime(field.split('-')[1],'%H:%M').strftime('%I:%M%p')
+            else:
+                employe['name']=field
+        employees.append(employe)
+#print(employees)
 
-    mondays = (df.loc[df['monday']!='free',["name","monday"]]) #separa al dia lunes y muestra los nombres de trabajadores y horario, excepto si esta "free"
-    """     print(mondays['monday'].isin(['10:00-12:00']))"""
-    data_frame = mondays.set_index('monday') #transforma la cadena de texto string a un conjunto de numeros (formato hora)
-    df_monday = data_frame[(data_frame.index > '10:00') & (data_frame.index <= '12:00')] # compara si hay valores dentro del dia que estan dentro de las horas 10:00-12:00 y muestra si es que existen cruces de trabajadores, sino no los muestra
-    print (df_monday)
-    print('-----------------------')
-    tuesday = (df.loc[df['tuesday']!='free',["name","tuesday"]]) #separa al dia lunes y muestra los nombres de trabajadores y horario, excepto si esta "free"
-    data_frame_tuesday = tuesday.set_index('tuesday') #transforma la cadena de texto string a un conjunto de numeros (formato hora)
-    df_tuesday = data_frame_tuesday[(data_frame_tuesday.index > '10:00') & (data_frame_tuesday.index <= '12:00')] # compara si hay valores dentro del dia que estan dentro de las horas 10:00-12:00 y muestra si es que existen cruces de trabajadores, sino no los muestra
-    print (df_tuesday)
-
-
-        
+for index,employe in enumerate (employees,1):
+    #print('pivote'+ employe['name'])
+    for other_employees in employees[index:]:
+        print(employe['name'] + '-' + other_employees['name'])
+        for key,value in employe.items():
+            if(key != 'name'):
+                if(employe[key] >= other_employees[key]) and (employe[key] <= other_employees[]):
