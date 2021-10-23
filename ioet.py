e2ccd82ec0ca1005584d7ce33c319a044ed1c77f
ElJeringas <santiagojss32@gmail.com>
2021-10-22 21:51:18 -0500

    ioet excercise solution

--- ioet.py
+++ ioet.py
@@ -15,7 +15,6 @@ end_date2=""
 
 with open('c:/Users/Santiago/Desktop/SantiagoSarmiento_Ioet/schedule.txt',newline='') as file:  #open the .txt file going to the directory  and name this variable 'file'
     csv_reader = csv.reader(file, delimiter=',') #convert the .txt in csv format, this will transform the text to table
-    #header=next(csv_reader)
     for row in csv_reader:
         employe={} #diccionario
         for field in row:
@@ -28,30 +27,26 @@ with open('c:/Users/Santiago/Desktop/SantiagoSarmiento_Ioet/schedule.txt',newlin
 #print(employees)
 
 for index,employe in enumerate (employees,1):
+    
     #print('pivote'+ employe['name'])
     for other_employees in employees[index:]:
         #print(employe['name'] + '-' + other_employees['name'])
+        cont=0
         for key,value in employe.items():
-            if(key != 'name'):
-                #print(employe['name'] + '-' + other_employees['name'],key,value)
-                print('----------------')
+            
             for key2,value2 in other_employees.items():
                 if(key2 != 'name'):
-                    
-                    if(key=='MO_start') and (key2=='MO_start') :
-                        print ('entrada' + employe['name'] + '-' + other_employees['name'],value+ '-' + value2)
-                        start_date1=value
-                        start_date2=value2
+                    if(key==key2):
+                        if("_start" in key and "_start" in key2):
+                           # print ('entrada' + employe['name'] + '-' + other_employees['name'],value+ '-' + value2)
+                            start_date1=value
+                            start_date2=value2
+
                         #(StartDate1 <= EndDate2) and (StartDate2 <= EndDate1)
-                    if(key=='MO_end') and (key2=='MO_end') :
-                        print ('salida' + employe['name'] + '-' + other_employees['name'],value+ '-' + value2)  
-                        end_date1=value                
-                        end_date2=value2 
-                        #print(end_date1)
-                       # print(start_date1)
-                       #datetime.strptime(start_date1, '%H:%M').strftime('%I:%M%p')
-                        if((datetime.strptime(start_date1, '%H:%M:%S').strftime('%H:%M:%S') <= datetime.strptime(end_date2, '%H:%M:%S').strftime('%H:%M:%S')) and (datetime.strptime(start_date2, '%H:%M:%S').strftime('%H:%M:%S') <= datetime.strptime(end_date1, '%H:%M:%S').strftime('%H:%M:%S'))):
-                            cont = cont + 1
-                            print (employe['name'] + '-' + other_employees['name'] , cont)   
-                            cont=0              
-                #print(key2,value2)
+                        if("_end" in key and "_end" in key2):
+                            #print ('salida' + employe['name'] + '-' + other_employees['name'],value + '-' + value2)  
+                            end_date1=value                
+                            end_date2=value2 
+                            if((datetime.strptime(start_date1, '%H:%M:%S').strftime('%H:%M:%S') <= datetime.strptime(end_date2, '%H:%M:%S').strftime('%H:%M:%S')) and (datetime.strptime(start_date2, '%H:%M:%S').strftime('%H:%M:%S') <= datetime.strptime(end_date1, '%H:%M:%S').strftime('%H:%M:%S'))):
+                                cont=cont+1
+        print (employe['name'] + '-' + other_employees['name'] , cont)   
